4e667e0c2664eced0ab81750848be2209f55b390
Katy Chadwell <kchadwell0226@gmail.com>
2022-03-05 20:51:00 -0600

    initialized spring boot app

---
+++ src/test/java/katyspringframework/mypetclinic/MyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package katyspringframework.mypetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
